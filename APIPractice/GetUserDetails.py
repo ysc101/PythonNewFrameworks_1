cb6a554ba6cc7b7003286636f756f51a1609e0e9
ysc101 <yogesh.chavhan@prmgroup.in>
2024-12-06 11:25:20 +0530

    MY Commit

--- APIPractice/GetUserDetails.py
+++ APIPractice/GetUserDetails.py
@@ -1,16 +1,18 @@
 import requests
-url="http://192.168.0.162:8082/api/LoginApi/GetUserDetails"
-params={
+def api_test():
+    url="http://192.168.0.162:8082/api/LoginApi/GetUserDetails"
+    params={
         "UserName":"AUDITWORK4",
         "PassWord":"Pass@123"
-}
-response=requests.get(url,params=params)
-if response.status_code==200:
-    try:
+    }
+    response=requests.get(url, params=params)
+    if response.status_code==200:
+       try:
         data=response.json()
         print("Response Data: ",data)
-    except requests.exceptions.JSONDecodeError:
+       except requests.exceptions.JSONDecodeError:
         print("Response not in json")
-else:
-    print("Response Status Code: ",response.status_code)
-    print(response.text)
+    else:
+       print("Response Status Code: ",response.status_code)
+       print(response.text)
+api_test()
