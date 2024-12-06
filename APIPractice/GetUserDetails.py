b7693be2cfd2b7eb574e14ebc59c9a2dac1c6ac4
ysc101 <yogesh.chavhan@prmgroup.in>
2024-12-06 10:50:20 +0530

    MY LPRS PROJECT FOR PYTHON AUTOMATION

---
+++ APIPractice/GetUserDetails.py
@@ -0,0 +1,16 @@
+import requests
+url="http://192.168.0.162:8082/api/LoginApi/GetUserDetails"
+params={
+        "UserName":"AUDITWORK4",
+        "PassWord":"Pass@123"
+}
+response=requests.get(url,params=params)
+if response.status_code==200:
+    try:
+        data=response.json()
+        print("Response Data: ",data)
+    except requests.exceptions.JSONDecodeError:
+        print("Response not in json")
+else:
+    print("Response Status Code: ",response.status_code)
+    print(response.text)
